c47944eba02e3e9d79b8d29a4df12c1af33be074
Vincent <vin.krae@gmail.com>
2026-01-19 11:31:31 +0100

    Fixed: Bugs in reference implementation Fixed: Bug in optimized implementation Refactored: Removed unnecessary declassify

--- src/crypto_sign/ed25519/amd64/ref/open.c
+++ src/crypto_sign/ed25519/amd64/ref/open.c
@@ -15,24 +15,24 @@ int crypto_sign_open(
   const unsigned char *pk
 )
 {	
-	uint8_t sig[64];
-	uint8_t pk_copy[32];
-	uint64_t msg_len = smlen - 64;
-	uint64_t res;
-	
-	memmove(pk_copy,pk,32);
-	memmove(sig,sm,64);
-	memmove(m,sm+64,msg_len);
-	memset(m + smlen - 64,0,64);
-	
-	res = jade_ed25519_amd64_verify(sig, pk_copy, m, msg_len);
+  uint8_t sig[64];
+  uint8_t pk_copy[32];
+  uint64_t msg_len = smlen - 64;
+  uint64_t res;
+
+  memmove(pk_copy,pk,32);
+  memmove(sig,sm,64);
+  memmove(m,sm+64,msg_len);
+  memset(m + smlen - 64,0,64);
+
+  res = jade_ed25519_amd64_verify(sig, pk_copy, m, msg_len);
   if (res == 0) {
     *mlen = msg_len;
     return 0;
   }
   else {
-  	*mlen = -1;
-  	memset(m,0,smlen);
-  	return -1;
+    *mlen = -1;
+    memset(m,0,smlen);
+    return -1;
   }
 }
